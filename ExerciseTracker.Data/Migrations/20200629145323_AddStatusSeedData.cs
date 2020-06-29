61fec311e2e8af862164cae378fbd594decbe23b
Marko Knezevic <markoknezevic75@gmail.com>
2020-06-29 17:46:34 +0200

    Create AddStatusSeedData Migration

---
+++ ExerciseTracker.Data/Migrations/20200629145323_AddStatusSeedData.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using ExerciseTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExerciseTracker.Data.Migrations
+{
+    public partial class AddStatusSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (int) Statuses.Undefined,
+                Name = Statuses.Undefined.GetType().GetField(Statuses.Undefined.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName
+
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (int) Statuses.Active,
+                Name = Statuses.Active.GetType().GetField(Statuses.Active.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName
+
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (short) Statuses.Inactive,
+                Name = Statuses.Inactive.GetType().GetField(Statuses.Inactive.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName
+
+            }).InsertSql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (short) Statuses.Undefined
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (short) Statuses.Active
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Status()
+            {
+                Id = (short) Statuses.Inactive                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+            }).DeleteSql);
+        }
+    }
+}
