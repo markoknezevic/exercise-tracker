deacb36a55d6a644fd72c44ccf0bb672bbf43cf1
Marko Knezevic <markoknezevic75@gmail.com>
2020-07-17 12:04:48 +0200

    Add seed data to exercise table and create migration

--- ExerciseTracker.Data/Entities/Exercise.cs
+++ ExerciseTracker.Data/Entities/Exercise.cs
@@ -1,9 +1,39 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExerciseTracker.Data.Entities
 {
+    public enum Exercises : long
+    {
+        [DisplayName("Push-Up")]
+        [Description("By raising and lowering the body using the arms, push-ups exercise the pectoral muscles, triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, serratus anterior, coracobrachialis and the midsection as a whole.")]
+        PushUp = 1,
+        
+        [DisplayName("Squat")]
+        [Description("A squat is a strength exercise in which the trainee lowers their hips from a standing position and then stands back up.")]
+        Squat = 2,
+        
+        [DisplayName("Sit-Up")]
+        [Description("The sit-up is an abdominal endurance training exercise to strengthen, tighten and tone the abdominal muscles.")]
+        SitUp = 3,
+        
+        [DisplayName("Chest Press")]
+        [Description("Sit on the glideboard with your knees bent and hold the handles with your hands on each side of your chest, palms facing down and elbows bent.Slide the glideboard up by pushing on the handles straight forward and allow yourself back down after a short pause.Keep your forearms parallel to the floor throughout.")]
+        ChestPress = 4,
+        
+        [DisplayName("Leg Curl")]
+        [Description("Lie on your back on the glideboard with your legs extended and attach your ankles to the wing attachment. Slide the glideboard up by curling your legs and allow yourself back down after a short pause. Breathe out while curling your legs and breathe in while returning to starting position.")]
+        LegCurl = 5,
+        
+        [DisplayName("Shoulder Press")]
+        [Description("Lie face down on the glideboard, knees bent and grasp the press-up bars with your hands, elbows bent. Slide the glideboard up by extending your arms (pushing yourself up) and allow yourself back down after a short pause. Breathe out while pushing and breathe in while returning to starting position.")]
+        ShoulderPress = 6,
+        
+        
+    }
+    
     [Table("exercises")]
     public class Exercise : TimestampedEntity<long>
     {
---
+++ ExerciseTracker.Data/Migrations/20200717090521_AddExerciseSeedData.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using ExerciseTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExerciseTracker.Data.Migrations
+{
+    public partial class AddExerciseSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.Squat,
+                Name = Exercises.Squat.GetType().GetField(Exercises.Squat.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.Squat.GetType().GetField(Exercises.Squat.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.ChestPress,
+                Name = Exercises.ChestPress.GetType().GetField(Exercises.ChestPress.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.ChestPress.GetType().GetField(Exercises.ChestPress.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.LegCurl,
+                Name = Exercises.LegCurl.GetType().GetField(Exercises.LegCurl.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.LegCurl.GetType().GetField(Exercises.LegCurl.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.PushUp,
+                Name = Exercises.PushUp.GetType().GetField(Exercises.PushUp.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.PushUp.GetType().GetField(Exercises.PushUp.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.ShoulderPress,
+                Name = Exercises.ShoulderPress.GetType().GetField(Exercises.ShoulderPress.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.ShoulderPress.GetType().GetField(Exercises.ShoulderPress.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.SitUp,
+                Name = Exercises.SitUp.GetType().GetField(Exercises.SitUp.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false).Cast<DisplayNameAttribute>()
+                    .FirstOrDefault()
+                    ?.DisplayName,
+                Description = Exercises.SitUp.GetType().GetField(Exercises.SitUp.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>()
+                    .FirstOrDefault()
+                    ?.Description
+            }).InsertSql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.Squat,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.ChestPress,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.LegCurl,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.LegCurl,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.PushUp,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.ShoulderPress,
+            }).DeleteSql);
+            
+            migrationBuilder.Sql((new Exercise()
+            {
+                Id = (long) Exercises.SitUp,
+            }).DeleteSql);
+        }
+    }
+}
